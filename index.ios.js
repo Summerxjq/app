8a77ea96b8831e9559a5bf647bd001a8ca2b8c91
xjqSummer <aaabbbxjq@163.com>
2017-12-13 17:53:18 +0800

    三次提交

--- index.ios.js
+++ index.ios.js
@@ -16,81 +16,87 @@ import {
     ScrollView,
     TouchableOpacity,
     TouchableHighlight,
+    Modal,
+    Button,
 } from 'react-native';
 const {width, height} = Dimensions.get('window');
 class AppTest extends Component {
     constructor(props){
         super(props);
-
-        var ds = new ListView.DataSource({rowHasChanged:(r1,r2) => r1 !==r2});
-        this.state={
-            dataSource:ds.cloneWithRows(['全部'])
-
-        };
-        //fetch('https://mywebsite.com/mydata.json')
+        this.state = {modalVisible:false};
+    }
+    setModalVisible(){
+        this.setState({modalVisible:true});
     }
 
-    getMoviesFromApiAsync = () => {
-        return fetch('https://facebook.github.io/react-native/movies.json')
-            .then((response) => response.json())
-            .then((responseJson) => {
-                //return responseJson.movies;
-                console.log(JSON.stringify(responseJson.movies));
-                let datas = responseJson.movies;
-                this.updateView(datas);
-            })
-            .catch((error) => {
-                console.error(error);
-            });
+    hideModel(){
+        this.setState({modalVisible:false});
     }
 
 
-    updateView = (array) => {
-        console.log(array);
-        var ds = new ListView.DataSource({rowHasChanged:(r1,r2) => r1 !==r2});
-        this.setState({
-            dataSource: ds.cloneWithRows(array)
-        });
-    };
+    render() {
+
+        return(
+
+            <View>
+
+                <View style={{flexDirection:'column'}}>
+
+                    <View style={{width:375,height:75,backgroundColor:"#05c5c2"}}>
+                        <Text style={{fontSize:18,color:"white",marginTop:40,marginLeft:150}}>账户管理</Text>
+                    </View>
+                    <View style={{width:375,height:45,backgroundColor:"#48d1cc"}} >
+                        <Text style={{fontSize:15,color:"white",lineHeight:45,marginLeft:20}}>账户号码：43677450036808888</Text>
+                    </View>
+                    <View style={{width:375,height:130,backgroundColor:"#05c5c2"}} >
+                        <Text style={{fontSize:12,color:"white",marginTop:30,marginLeft:150}}>账户总额（元）</Text>
+                        <Text style={{fontSize:30,color:"white",marginTop:5,marginLeft:90}}>1,500,000,00</Text>
+                    </View>
+                    <View style={{flexDirection:'row',width:375,height:60,backgroundColor:"#20b2aa"}}>
+                        <View style={{flexDirection:'column',marginLeft:20,marginTop:10}}>
+                            <Text style={{color:"white",fontSize:15}}>可用余额(元)</Text>
+       [                           <Text style={{color:"white",fontSize:18}}>200,00</Text>
+                        </View>
+                        <View style={{flexDirection:'column',marginLeft:100,marginTop:10}}>
+                            <Text style={{color:"white",fontSize:15}}>冻结余额(元)</Text>
+                            <Text style={{color:"white",fontSize:18}}>100,00</Text>
+                        </View>
+                    </View>
+                    <View style={{width:375,height:40,backgroundColor:"#dcdcdc"}}>
+                        <TouchableOpacity onPress={()=>{this.setModalVisible()}}>
+                            <Text style={{color:"#a9a9a9",marginLeft:20,marginTop:10,color:"black"}}>账户管理</Text>
+                        </TouchableOpacity>
+                    </View>
+
+                <Modal
+                    animationType={"slide"}
+                    transparent={true}
+                    visible={this.state.modalVisible}
+                    onRequestClose={() => {alert("Modal has been closed.")}}
+                >
+
+                       <View style={{width:260,height:180,backgroundColor:"white",marginTop:260,marginLeft:55}}>
+                           <Text style={{fontSize:20,marginLeft:110,marginTop:20}}>提示</Text>
+                           <Text style={{fontSize:15,marginLeft:30,marginTop:10}}>企业服务时间为9：00-19：00</Text>
+                           <Text style={{fontSize:15,marginLeft:50,marginTop:10}}>请在企业服务时间内操作</Text>
 
 
-   render(){
-       return(
-           <View>
-               <View style={{width:375,height:80,backgroundColor:"lightseagreen"}}>
-                   <Text style={{lineHeight:80,marginLeft:170,color:"white",fontSize:20}}>圈子</Text>
-               </View>
-               <View style={{width:375,height:10,backgroundColor:"gainsboro"}}></View>
-               <ListView style={{width:750}}
-                   dataSource={this.state.dataSource}
-                   renderRow={this.renderRow}
+                       <Text onPress={()=>{this.hideModel()}} style={{fontSize:18,marginLeft:100,marginTop:50,color:"green"}}>知道了</Text>
+                       </View>
 
-                   />
-               <View style={{width:375,height:400,backgroundColor:"gainsboro"}}></View>
-           </View>
-       )
+                </Modal>
 
-   }
 
-    renderRow = (rowData) => {
-       console.log(rowData);
-        return (
-        <View style={{height: 44}}>
-            <TouchableOpacity onPress={this.getMoviesFromApiAsync}>
-                <Text style={{marginTop:13,marginBottom:14,marginLeft:15,fontSize:15}}>{rowData.title==undefined? rowData:rowData.title}</Text>
-            </TouchableOpacity>
-            <View style={{width:375,height:1,backgroundColor:"gainsboro"}}></View>
-            <Image source={require('./image/p7.png')} style={{width:22,height:11,marginLeft:345,marginTop:-28}}></Image>
-        </View>
 
-        )
-    };
+                    </View>
+
+
+            </View>
+
+        );
+    }
+
 }
-
-
-
-
-
 
 
 AppRegistry.registerComponent('AppTest', () => AppTest);
